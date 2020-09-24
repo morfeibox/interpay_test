0eb794db801f89a5990b51ce67615210a172bfd9
Atanas Beychev <abeychev@dd-fs.co.uk>
2020-09-24 14:30:39 +0300

    Display result in a table, add CSS for styling

--- includes/ViewBook.php
+++ includes/ViewBook.php
@@ -7,13 +7,29 @@ class ViewBook extends Book
     {
         $authors = $this->getSearchedAuthors($search);
         if(!is_array($authors)){
-            echo 'There is no book with Author  ' . trim($search);
+            echo "<div class=\"message\"> There is no book with author  <b>\"" . trim($search) . "\"</b></div>";
+        } else {
+            echo "<table>
+                        <thead>
+                            <tr>
+                                <th>Author</th>
+                                <th>Book</th>
+                                <th>Location</th>
+                                <th>Created On</th>
+                            </tr>
+                        </thead>
+                   <tbody>";
+            foreach ($authors as $author) {
+                echo "<tr>";
+                echo "<td>" . $author['author'] . "</td>";
+                echo "<td>" . $author['book'] . "</td>";
+                echo "<td>" . $author['file_path'] . '/' . $author['file_name'] . "</td>";
+                echo "<td>" . $author['created_on'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</tbody>";
+            echo "</table>";
         }
-        foreach ($authors as $author) {
-            echo $author['author'] . "<br>";
-            echo $author['book'] . "<br>";
-            echo $author['file_path'] . "<br>";
-            echo $author['file_name'] . "<br>";
-        }
+
     }
 }
--- index.php
+++ index.php
@@ -12,25 +12,116 @@ include 'includes/ViewBook.php';
     <meta name="description" content="Demo Project">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style type="text/css"></style>
+    <style>
+        .container {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+        }
+
+        .section-form {
+            width: 100%;
+            margin-bottom: 30px;
+        }
+
+        .section-table {
+            width: 100%;
+        }
+
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+            background: #49a09d;
+            font-family: sans-serif;
+            font-weight: 100;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            overflow: hidden;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+        }
+
+        th, td {
+            padding: 15px;
+            background-color: rgba(255, 255, 255, 0.2);
+            color: #fff;
+        }
+
+        th {
+            text-align: left;
+        }
+
+        thead, th {
+            background-color: #55608f;
+        }
+
+        input[type=text], select {
+            width: 30%;
+            display: inline;
+            padding: 12px 20px;
+            margin: 10px 0 0 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        button {
+            width: 20%;
+            display: inline;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 10px 0 0 10px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+
+        .message {
+            width: 50%;
+            margin-left: 10px;
+            padding: 12px 0 12px 15px;
+            border-radius: 4px;
+            background-color: rgba(255, 255, 255, 0.2);
+            color: #fff;
+            font-weight: 100;
+        }
+    </style>
 </head>
 <body>
-<form action="" method="GET">
-    <input type="text" name="query" />
-    <input type="submit" name="submit" value="Search" />
-</form>
-<?php
-$books = new ViewBook();
-if(isset($_GET['submit'])){
-    if(empty($_GET['query'])){
-        echo "Enter a search term";
-    }
-    $query = $_GET['query'];
+<div class="container">
+    <div class="section-form">
+        <form action="" method="GET">
+            <input type="text" name="query"/>
+            <button type="submit" name="submit">Search</button>
+        </form>
+    </div>
+    <div class="section-table">
+        <?php
+        $books = new ViewBook();
+        if (isset($_GET['submit'])) {
+            if (empty($_GET['query'])) {
+                echo "<div class=\"message\">Enter a search term</div>";
+            }
+            $query = $_GET['query'];
 
-}
-if($query) {
-    echo $books->showSearchedAuthors($query);
-}
-?>
-
+        }
+        if ($query) {
+            echo $books->showSearchedAuthors($query);
+        }
+        ?>
+    </div>
+</div>
 </body>
 </html>
+
